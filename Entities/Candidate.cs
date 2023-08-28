0f329d60410586005b68cc4719733d8d36541091
Newton Duarte Cavalcanti <newton_@hotmail.com>
2023-08-28 19:37:31 -0300

    first commit

---
+++ Entities/Candidate.cs
@@ -0,0 +1,5 @@
+class Candidate
+{
+  public string Name { get; set; }
+  public int Votes { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿string filePath = "votes.txt";
+Dictionary<string, int> voting = new ();
+
+try
+{
+  using (var streamReader = File.OpenText(filePath))
+  {
+    while(!streamReader.EndOfStream)
+    {
+      string[] line = streamReader.ReadLine().Split(",");
+      var candidate = new Candidate { Name = line[0], Votes = int.Parse(line[1]) };
+      if (voting.ContainsKey(candidate.Name)) {
+        voting[candidate.Name] += candidate.Votes;
+      } else {
+        voting[candidate.Name] = candidate.Votes;
+      }
+    }
+  }
+}
+catch (Exception e)
+{
+  System.Console.WriteLine(e.Message);
+}
+
+System.Console.WriteLine("Voting:");
+foreach(var item in voting)
+{
+  System.Console.WriteLine($"{item.Key}: {item.Value}");
+}
